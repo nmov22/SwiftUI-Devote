adda2eca7bc33837eae79d25386e3f4533f3e870
Noel Velasco <nvelasco@yondu.com>
2023-11-11 10:32:52 +0800

    - initial commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Noel Velasco on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
